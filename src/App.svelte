5097cb7dd553534ad728f73a609f156268cdfe39
b3b00 <olivier.duhart@gmail.com>
2024-01-14 09:09:33 +0100

    cleaning

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
 
 	let root = treeData;
 
-	let accessor = (x) => {
+	let childrenAccessor = (x) => {
 		if (x && x.children != undefined && x.children != null && Array.isArray(x.children)) {
             return x.children;
         }
@@ -20,32 +20,9 @@
 	let nodeId = (x) => x.id;
 	
 	let filter = (node, search) => node.name.includes(search);
-	// 		if (search === undefined || search === null || search == '') {
-	// 		return node;
-	// 	}
-	// 	var children = accessor(node);
-	// 	if (children.length > 0) {
-	// 		var filtered = children.map(x => nodefilter(x, search)).filter(x => x!= null);
-	// 		if ( node.name.includes(search)) {
-	// 			return node;
-	// 		}
-	// 		else if (filtered.length > 0) {
-	// 			return {name:node.name,
-	// 						 id:node.id,
-	// 						 children:filtered 
-	// 						 };
-	// 		}
-	// 		return null;
-	// 	}
-	// 	else {
-	// 		if (node.name.includes(search)) {
-	// 			return node;
-	// 		}
-	// 		return null;
-	// 	}
-	// }  
+	
 </script>
 {#if $selectedNode}
 <h1>#{$selectedNode.id} {$selectedNode.name}</h1>	
 {/if}
-<TreeView emptyTreeMessage="no super hero to show" selectable root={root} childrenAccessor={accessor} nodeTemplate={Node} {filter} {nodeId}></TreeView>
+<TreeView emptyTreeMessage="no super hero to show" selectable {root} {childrenAccessor} nodeTemplate={Node} {filter} {nodeId}></TreeView>
